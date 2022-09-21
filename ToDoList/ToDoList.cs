a11d648ed435bf1f061d3cb75073e8278893588a
Noah Lundquist <nalundquist@gmail.com>
2022-09-21 10:00:36 -0700

    add UI to existing backend (WIP)
    Co-authored-by: Joseph Jackson <josephjacksonw@gmail.com>

---
+++ ToDoList/ToDoList.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			int number = 0;
+			bool consoleLoop = true;
+			while (consoleLoop)
+			{
+				Console.WriteLine("Welcome to the To Do List!");
+				Console.WriteLine("Would you like to 'Add' and item to your list or 'View' your list? Or 'Q' to quit");
+				string userInput1 = Console.ReadLine();
+				if (userInput1.ToLower() == "add")
+				{
+					Console.WriteLine("Input the list item you would like displayed.");
+					string userInput2 = Console.ReadLine();
+					Item listItem = new Item(userInput2);
+				}
+				else if (userInput1.ToLower() == "view")
+				{
+					List<Item> ToDoList = new List<Item>.GetAll();
+					foreach (Item toDo in ToDoList)
+					{
+						number += 1;
+						Console.WriteLine(number + ". " + toDo);
+					}
+				}
+				else if (userInput1.ToLower() == "q")
+				{
+					consoleLoop = false;
+				}
+				else
+				{
+					Console.WriteLine("Your input is invalid.");
+				}
+			}
+			
+		}
+	}
+}
